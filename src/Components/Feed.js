8d56e293720f768b3fb4c788e8bc19c7f0ccf62d
Sean Joo <hyunseo.joo@berkeley.edu>
2022-11-07 17:42:40 -0800

    save

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,20 +2,23 @@ import { useState } from 'react';
 import React from 'react';
 import Menu from './Menu';
 import Block from './Block';
-
+import Color from './Color.js';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
-
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
-
+    // const = [variabletoUpdate, functionToUpdateVariable] = useState(initialValue);
+    const [posts, updateState] = useState([]); // TODO: edit this variable
     return (
         <div>
-            <Menu></Menu>
-
+            {/* when clicking post "color" block, then add to feed */}
+            <Menu posts={posts} updateState={updateState}/>
+            {/* <Menu></Menu> */}
             {/* Below is where all of your Blocks should render! */}
-            {posts}
+            {posts.map((backgroundColor) => {
+                <Block color={backgroundColor}/>
+            })}
+            {/* {posts} */}
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Color from './Color';
+
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="red" handleClick={props.clickFunc}/>
+          <Color color="pink" handleClick={props.clickFunc}/>
+          <Color color="blue" handleClick={props.clickFunc}/>
+          <Color color="green" handleClick={props.clickFunc}/>
       </div>
     );
 }
